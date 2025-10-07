2922e6da28a94cd94d1228437904399d9fbc2a9e
EvanL2021 <evan.labit2@gmail.com>
2025-10-07 22:11:30 +0200

    Upgraded angular core to 18

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -5,14 +5,18 @@ import { FooterComponent } from './footer/footer.component';
 import { PresentationComponent } from './presentation/presentation.component';
 import { CompetencesComponent } from './competences/competences.component';
 import { ContactComponent } from './contact/contact.component';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 import { ParcoursComponent } from './parcours/parcours.component';
 import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, PresentationComponent, CompetencesComponent, ContactComponent, HttpClientModule, ParcoursComponent, ProjectsComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, PresentationComponent, CompetencesComponent, ContactComponent, 
+// TODO: `HttpClientModule` should not be imported into a component directly.
+// Please refactor the code to add `provideHttpClient()` call to the provider list in the
+// application bootstrap logic and remove the `HttpClientModule` import from this component.
+HttpClientModule, ParcoursComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
